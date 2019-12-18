de531f8f085f87c10bfeadef517290d3ffa5b7e7
kacky <t_kaki@nextappli.com>
2019-12-18 10:30:34 +0900

    add: first commit

---
+++ player.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+export DISPLAY=:0
+
+wget http://raw.githubusercontent.com/coderholic/pyradio/master/pyradio/stations.csv -O stations.csv >& wget_output
+
+stations=()
+
+while read row; do
+  column=`echo ${row} | cut -d , -f 2`
+  stations+=(${column})
+done < stations.csv
+
+index=17 # $(($RANDOM % ${#stations[@]}))
+
+mplayer -playlist ${stations[${index}]} > player_output 2> player_error < /dev/null &
+
+exit 0
