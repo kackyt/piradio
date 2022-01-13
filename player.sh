02131f7cde9a18c2cf90606c720b433399f46c44
kacky <t_kaki@nextappli.com>
2022-01-13 13:57:40 +0900

    feat: temp

--- player.sh
+++ player.sh
@@ -3,7 +3,7 @@
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 export DISPLAY=:0
 
-wget http://raw.githubusercontent.com/coderholic/pyradio/master/pyradio/stations.csv -O stations.csv >& wget_output
+# wget http://raw.githubusercontent.com/coderholic/pyradio/master/pyradio/stations.csv -O stations.csv >& wget_output
 
 stations=()
 
@@ -12,11 +12,11 @@ while read row; do
   stations+=(${column})
 done < stations.csv
 
-candidates=(10 14 16 18 23)
+#candidates=(10 14 18 23)
 
-ind=$(($RANDOM % ${#candidates[@]}))
+index=$(($RANDOM % ${#stations[@]}))
 
-index=${candidates[${ind}]}
+#index=${candidates[${ind}]}
 
 mplayer -playlist ${stations[${index}]} > player_output 2> player_error < /dev/null &
 
--- timer.sh
+++ timer.sh
@@ -2,4 +2,6 @@
 
 cd $(dirname $0)
 
+sudo systemctl stop piradio
 mpg321 bell.mp3 -l `date '+%-I'` >& /dev/null
+sudo systemctl start piradio
